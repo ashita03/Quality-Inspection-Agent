1b34193f9e1b2f91e1fb97c2c2ed230dd449e109
ashita03 <ashitashetty1@gmail.com>
2026-09-07 00:37:07 -0400

    Frontend deployment script changes for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({ plugins: [react(), tailwindcss()], })
+
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    base: 'Quality-Inspection-Agent', // must match your GitHub repo name exactly
+})
